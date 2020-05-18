d20e0833b06e10c75dd103caa6491a1eb5d289d7
marb <marb61a@gmail.com>
2020-05-18 12:31:30 +0100

    added initial assertion test

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,17 @@
 describe("Ny First Test Suite", function(){
     it("My First Test Case", function(){
-        cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/");
+        cy.visit("https://www.rahulshettyacademy.com/seleniumPractise/#/");
+
+        // CYpress only supports CSS locators
+        cy.get('.search-keyword').type('ca');
+
+        // Waiting to give page elements a chance to load
+        cy.wait(2000)
+
+        // The Get in Cypress acts like the findElement method in Selenium
+        // Some elements may exist with the same name but be invisible, this
+        // will throw off counts and asserts thus failing tests
+        cy.get('.product:visible')
+            .should('have.length', 4)
     });
 });
